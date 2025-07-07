96e3674c22b7cc17a18184683c44f43f7a23582f
MUHAMMED SINAN D <carreersins@gmail.com>
2025-07-07 17:34:19 +0530

    okk

--- PostfixAnomaly/run.sh
+++ PostfixAnomaly/run.sh
@@ -27,7 +27,17 @@ echo
 
 # Activate conda environment
 echo "Activating conda environment py31010..."
-source ~/anaconda3/etc/profile.d/conda.sh || source ~/miniconda3/etc/profile.d/conda.sh
+# Try to find conda.sh in common locations
+if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+    source "$HOME/anaconda3/etc/profile.d/conda.sh"
+elif [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+    source "$HOME/miniconda3/etc/profile.d/conda.sh"
+elif [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
+    source "/opt/conda/etc/profile.d/conda.sh"
+else
+    # If conda.sh not found, try to use conda directly if it's in PATH
+    echo "Warning: conda.sh not found in standard locations, trying to use conda directly"
+fi
 conda activate py31010
 
 # Run the detector
